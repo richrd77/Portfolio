89720bfb3a89a3c1def5a9016aa1a51975a527b7
Richard <richrd77@gmail.com>
2024-08-03 16:02:14 +0530

    respnosive logo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,28 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'ric-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements AfterViewInit {
   @ViewChild('na') private navElement!: ElementRef<HTMLElement>;
+  @ViewChild('log') private logo!: ElementRef<HTMLImageElement>;
+
+  ngAfterViewInit(): void {
+    if (!!window.matchMedia) {
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        this.logo.nativeElement.src = 'assets/logo/logo-dark.png';
+        return;
+      } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+        this.logo.nativeElement.src = 'assets/logo/logo-light.png';
+        return;
+      }
+    }
+    this.logo.nativeElement.src = 'assets/logo/logo-accent.png';
+    return;
+  }
+
 
   public ShowMenu(event: MouseEvent): void {
     this.navElement.nativeElement.classList.add('show');
